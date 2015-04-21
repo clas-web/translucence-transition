3cc0281e4ef123f9f31f1d52344ada3912274335
Crystal Barton <cbarto11@uncc.edu>
2015-04-21 12:34:31 -0400

    clean up code.

--- main-network-display.php
+++ main-network-display.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Translucence Transition Network Display
+Plugin Name: Translucence Transition - Network Admin Page
 Plugin URI: 
 Description: 
 Version: 0.0.1
@@ -10,39 +10,38 @@ Network: True
 */
 
 
-if( !defined('TT') ):
+if( !defined('TTNAP') ):
 
-define( 'TT', 'Translucence Transition' );
+define( 'TTNAP', 'Translucence Transition - Network Admin Page' );
 
-define( 'TT_DEBUG', true );
+define( 'TTNAP_DEBUG', true );
 
-define( 'TT_PLUGIN_PATH', dirname(__FILE__) );
-define( 'TT_PLUGIN_URL', plugins_url('', __FILE__) );
+define( 'TTNAP_PLUGIN_PATH', dirname(__FILE__) );
+define( 'TTNAP_PLUGIN_URL', plugins_url('', __FILE__) );
 
-define( 'TT_VERSION', '0.0.1' );
-define( 'TT_DB_VERSION', '1.0' );
+define( 'TTNAP_VERSION', '0.0.1' );
+define( 'TTNAP_DB_VERSION', '1.0' );
 
-define( 'TT_VERSION_OPTION', 'tt-version' );
-define( 'TT_DB_VERSION_OPTION', 'tt-db-version' );
+define( 'TTNAP_VERSION_OPTION', 'tt-version' );
+define( 'TTNAP_DB_VERSION_OPTION', 'tt-db-version' );
 
-define( 'TT_OPTIONS', 'tt-options' );
-define( 'TT_LOG_FILE', dirname(__FILE__).'/log.txt' );
+define( 'TTNAP_OPTIONS', 'tt-options' );
 
 endif;
 
 
 if( is_admin() ):
 
-add_action( 'admin_enqueue_scripts', array('TT_Display', 'enqueue_scripts') );
+add_action( 'admin_enqueue_scripts', array('TTNAP_Main', 'enqueue_scripts') );
 require_once( dirname(__FILE__).'/apl/apl.php' );
-add_action( 'wp_loaded', array('TT_Display', 'load') );
-add_action( 'network_admin_menu', array('TT_Display', 'update'), 5 );
+add_action( 'wp_loaded', array('TTNAP_Main', 'load') );
+add_action( 'network_admin_menu', array('TTNAP_Main', 'update'), 5 );
 
 endif;
 
 
-if( !class_exists('TT_Display') ):
-class TT_Display
+if( !class_exists('TTNAP_Main') ):
+class TTNAP_Main
 {
 	
 	public static function enqueue_scripts()
@@ -54,24 +53,17 @@ class TT_Display
 	{
 		require_once( dirname(__FILE__).'/admin-pages/require.php' );
 		
-		// Network admin pages.
 		$pages = new APL_Handler( true );
-		
 		$pages->add_page( new TT_ThemeListAdminPage );
-		
 		$pages->setup();
 	}
 	
 	
 	public static function update()
 	{
-//		$version = get_option( TT_DB_VERSION_OPTION );
-//  	if( $version !== TT_DB_VERSION )
-//  	{
- 			$model = TT_Model::get_instance();
- 			$model->create_tables();
-//  	}
- 		
+		$model = TT_Model::get_instance();
+		$model->create_tables();
+
  		update_option( TT_VERSION_OPTION, TT_VERSION );
  		update_option( TT_DB_VERSION_OPTION, TT_DB_VERSION );
 	}
