a15ab78aa9b08d6c25f2297e21f73c078ffcab7e
Crystal Barton <cbarto11@uncc.edu>
2015-04-29 16:30:35 -0400

    update activate jetpack page.

--- admin-pages/require.php
+++ admin-pages/require.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once( dirname(__FILE__).'/theme-list.php' );
-require_once( dirname(__FILE__).'/switch-theme.php' );
+require_once( dirname(__FILE__).'/activate-jetpack.php' );
 require_once( dirname(__FILE__).'/../model/model.php' );
 
